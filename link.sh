6b679b885b800b67a19bb5576e2f71d592b391ad
Matt Petters <matt@petters.io>
2017-04-10 10:39:49 -0400

    Adding link script

--- link.sh
+++ link.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd ~
 ln -s -f .dotfiles/vim/.vim_runtime
+ln -s -f .dotfiles/vim/.vimrc
+ln -s -f .dotfiles/tmux/.tmux.conf
+ln -s -f .dotfiles/bash/.bashrc
+ln -s -f .dotfiles/bash/.bash_profile
+ln -s -f .dotfiles/newsbeuter/.newsbeuter
