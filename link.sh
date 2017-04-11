341e68c419b57ac6f402f12c987ac897da9451c1
Matt Petters <matt@petters.io>
2017-04-11 17:49:38 -0400

    Adding tmuxinator startup script

--- link.sh
+++ link.sh
@@ -6,3 +6,4 @@ ln -s -f .dotfiles/tmux/.tmux.conf
 ln -s -f .dotfiles/bash/.bashrc
 ln -s -f .dotfiles/bash/.bash_profile
 ln -s -f .dotfiles/newsbeuter/.newsbeuter
+ln -s -f .dotfiles/tmux/.tmuxinator ~/.tmuxinator
