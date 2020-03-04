f9a235bfa101fb5e0a0a3077c8590e62207ff485
Matt Petters <mpetters@coca-cola.com>
2020-03-04 14:00:54 -0500

    adding better matrix script

--- shells/scripts/aliases.sh
+++ shells/scripts/aliases.sh
@@ -8,7 +8,7 @@ alias ll='ls -la'
 alias mvim="/Applications/MacVim.app/contents/MacOS/MacVim"
 alias gac="git add --all && git commit"
 alias gits="git status"
-alias matrix='LC_ALL=C tr -c "[:digit:]" " " < /dev/urandom | dd cbs=$COLUMNS conv=unblock | GREP_COLOR="1;32" grep --color "[^ ]"'
+alias matrix="./matrix.sh"
 # gh_create() { 
 #     echo $@
 #     curl -u 'mattpetters' https://api.github.com/user/repos -d "{\"name\":\"$@\"}"
