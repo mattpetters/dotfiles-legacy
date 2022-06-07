5b28ab5e82259477957ce5a8ba96dc5222348dd4
Matt Petters <mcpetters@gmail.com>
2022-06-07 05:48:39 +0000

    time in shell

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install chain
 
+set TZ America/Los_Angeles
+
 function time_link
   echo yellow
-  date '+%H:%M:%S'
+  date '%r %Z'
 end
 
 chain.push time_link
