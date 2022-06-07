df9d61eead76a8ac99abc25a493b77b0c1d1b755
Matt Petters <mcpetters@gmail.com>
2022-06-07 07:10:44 +0000

    dotfiles update

--- install.sh
+++ install.sh
@@ -9,3 +9,25 @@ function time_link
 end
 
 chain.push time_link
+
+brew install fzf
+
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+sudo apt install fd-find
+
+sudo apt install kubectx
+
+# somehow install nerd font
+# https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+
+curl -sS https://starship.rs/install.sh | sh
+
+# ~/.config/fish/config.fish
+
+touch ~/.config/fish/config.fish
+
+echo "starship init fish | source" >> ~/.config/fish/config.fish
+
+brew install git-delta
