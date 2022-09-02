b64dca6459bf72eb7a0ea5347f9b7683358755f3
Matt Petters <mcpetters@gmail.com>
2022-09-02 12:19:57 -0700

    fish utility functions

--- fish/fns.fish
+++ fish/fns.fish
@@ -5,3 +5,15 @@ end
 function glm
     git checkout master && git pull 
 end
+
+function gac
+    git add --all && git commit
+end
+
+function cf
+    cd ~/code
+end
+
+function dprune
+    docker system prune --all
+end
