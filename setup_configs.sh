c47dd6effec2b0b4e44d2e562d60d2f3e3053722
Matt Petters <matthew.petters@safely.com>
2023-06-26 09:02:57 -0700

    oh my zsh

--- setup_configs.sh
+++ setup_configs.sh
@@ -1,6 +1,9 @@
 # setup zshrc
 source $HOME/.dotfiles/zsh/link_zsh.sh
 
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # set up NvChad
 # https://nvchad.com/docs/quickstart/install
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
