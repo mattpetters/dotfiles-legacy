0313c1d66d6da7be3fa993f757d7827704ce8b37
Matt Petters <matthew.petters@safely.com>
2023-06-26 09:03:45 -0700

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -4,6 +4,7 @@ alias vi="nvim"
 alias oldvim="vim"
 alias gac="git add --all && git commit"
 alias gits="git status"
+alias cf="cd ~/code"
 
 dka(){
     docker stop $(docker ps -a -q); docker rm $(docker ps -a -q);
