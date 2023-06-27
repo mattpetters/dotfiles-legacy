13abc04ec992190bbf6efa24796897e7019ae83d
Matt Petters <matthew.petters@safely.com>
2023-06-27 07:59:32 -0700

    update dotfiles alias

--- aliases.sh
+++ aliases.sh
@@ -5,6 +5,7 @@ alias oldvim="vim"
 alias gac="git add --all && git commit"
 alias gits="git status"
 alias cf="cd ~/code"
+alias dots="cd ~/.dotfiles"
 
 dka(){
     docker stop $(docker ps -a -q); docker rm $(docker ps -a -q);
