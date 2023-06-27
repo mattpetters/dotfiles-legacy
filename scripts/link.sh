2e0025eda0a87a12325c2e2656ebe38ce7c6ccc9
Matt Petters <matthew.petters@safely.com>
2023-06-27 08:37:46 -0700

    comment out old stuff

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #TODO: Make sure the software is installed first, if not install it
 cd ~
-ln -s -f .dotfiles/vim/.vim_runtime
-ln -s -f .dotfiles/vim/.vimrc
+# ln -s -f .dotfiles/vim/.vim_runtime
+# ln -s -f .dotfiles/vim/.vimrc
 ln -s -f .dotfiles/tmux/.tmux.conf
 ln -s -f .dotfiles/bash/.bashrc
 ln -s -f .dotfiles/bash/.bash_profile
-ln -s -f .dotfiles/newsbeuter/.newsbeuter
+# ln -s -f .dotfiles/newsbeuter/.newsbeuter
 ln -s -f .dotfiles/tmux/.tmuxinator ~/.tmuxinator
 ln -s -f .dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
